264efb4e55a058f80da80de074e744177a1439ae
msaimadh <msaimadh@IN-MUM-LDVM035.corp.capgemini.com>
2018-10-08 09:38:21 -0400

    firstcommit

---
+++ src/main/java/com/capgemini/productapp/service/ProductService.java
@@ -0,0 +1,22 @@
+package com.capgemini.productapp.service;
+
+import com.capgemini.productapp.entity.Product;
+import com.capgemini.productapp.exception.ProductNotFoundException;
+
+public interface ProductService {
+
+	public Product addProduct(Product product);
+
+	public Product updateProduct(Product product);
+
+	public Product findProductById(int productId) throws ProductNotFoundException;
+
+	public void deleteProduct(Product product);
+	
+/*	public Product findProductByName(String productName);
+	
+	public Product findProductBycategory(String productCategory); */
+
+}
+
+
